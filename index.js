189f34862026978ebcdd076e57a82500f34c3675
backup0 <backupemailaddress@protonmail.com>
2021-12-15 17:25:58 +0200

    Change index.js file.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3001
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
-
-console.log('Hello from console)))')
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
